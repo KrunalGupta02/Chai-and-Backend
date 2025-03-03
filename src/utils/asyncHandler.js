d4bb552ce31f12f85e75d087345a95bc9051d9ea
KrunalGupta02 <guptakrunal02@gmail.com>
2025-03-03 16:21:13 +0530

    added useful comments

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,10 @@
+//? What is asyncHandler?
+// asyncHandler is a higher-order function.
+// A higher-order function is a function that takes another function as an argument and possibly returns a new function.
+
+//?Why is asyncHandler needed?
+// In Express.js, if an asynchronous function throws an error, you need to explicitly pass that error to the next function for the Express error-handling middleware to catch it. Without this, your server might crash or not handle the error properly.
+
 const asyncHandler = (requestHandler) => {
   (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
@@ -6,6 +13,7 @@ const asyncHandler = (requestHandler) => {
 
 export { asyncHandler };
 
+// Different way of writing same above async handler
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
 //       await fn(req,res,next)
