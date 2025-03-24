775901da914103a7e2c55e3afec78e816ec0b52a
Krunal Gupta <guptakrunal02@gmail.com>
2025-03-24 15:59:45 +0530

    Add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { deleteFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 //? This is just for example for test in postman
 // const registerUser = asyncHandler(async (req, res) => {
@@ -469,6 +470,54 @@ const userChannelProfile = asyncHandler(async (req, res) => {
 
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user_id)
+      }
+    },
+    {
+      $lookup: {
+        from: 'vidoes',
+        localField: 'watchHistory',
+        foreignField: '_id',
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: 'owner',
+              foreignField: '_id',
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                },
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user[0].watchHistory, 'Watch History fetched successfully'))
+})
+
 export {
   registerUser,
   loginUser,
@@ -479,5 +528,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  userChannelProfile
+  userChannelProfile,
+  getWatchHistory
 };
