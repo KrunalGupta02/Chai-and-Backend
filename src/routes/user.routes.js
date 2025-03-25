411cd43f6ef876319530e87e7bdd1614c226bb4b
Krunal Gupta <guptakrunal02@gmail.com>
2025-03-25 10:38:44 +0530

    added user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getWatchHistory,
   loginUser,
   logOutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  userChannelProfile,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -31,5 +38,18 @@ router.route("/login").post(loginUser);
 // secured route
 router.route("/logout").post(verifyJWT, logOutUser); // injecting the authMiddleware before
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/current-user').get(verifyJWT, getCurrentUser);
+// Patch will only update the specific which is mentioned not all but
+// post will update the whole file
+router.route('/update-account').patch(verifyJWT, updateAccountDetails);
+router.route('/avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
+router.route('/cover-image').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
+
+// If data is taking from url-params
+router.route('/c/:username').get(verifyJWT, userChannelProfile);
+
+router.route('/history').get(verifyJWT, getWatchHistory)
 
 export default router;
