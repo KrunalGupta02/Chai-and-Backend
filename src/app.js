ec92fb9e13e91fdb3f25cb1d83e01a4d016ead7e
Krunal Gupta <guptakrunal02@gmail.com>
2025-03-27 22:26:31 +0530

    add assignments from yt

--- src/app.js
+++ src/app.js
@@ -25,11 +25,30 @@ app.use(cookieParser());
 // routes import
 import userRouter from "./routes/user.routes.js";
 
+
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
 // routes declaration
 //? Here we are using the middleware 'use' bcz we have separated the whole router and controller that's why we use the middleware not like app.get('/')
 
 // Route will be like that `https://localhost:8000/api/v1/users/login`
 
 app.use("/api/v1/users", userRouter);
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/vidoes", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
